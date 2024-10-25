69ab4922922ee37cbe392dece1588ba62466e915
Rich-Data-F <richard.bonfils.data@gmail.com>
2024-10-25 13:03:11 +0200

    GA4 tracker insertion

--- app.py
+++ app.py
@@ -25,12 +25,42 @@ import bcrypt
 from functools import wraps
 import sqlite3
 import hashlib
+import pathlib
+from bs4 import BeautifulSoup
+import shutil
 
 # User roles
 USER_ROLE = 'user'
 ADMIN_ROLE = 'admin'
 
 # Initialize SQLite database
+
+def inject_ga():
+    GA_ID="google_analytics"
+    GA_JS= """
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-Z0YKHBLBKY"></script> 
+    <script> 
+        window.dataLayer = window.dataLayer || []; 
+        function gtag(){dataLayer.push(arguments);} 
+        gtag('js', new Date());
+        gtag('config', 'G-Z0YKHBLBKY');
+        </script> 
+        """
+    # Insert the script in the head tag of the static template inside your virtual
+    index_path = pathlib.Path(st.__file__).parent / "static" / "index.html"
+    logging.info(f'editing {index_path}')
+    soup = BeautifulSoup(index_path.read_text(), features="html.parser")
+    if not soup.find(id=GA_ID): 
+        bck_index = index_path.with_suffix('.bck')
+        if bck_index.exists():
+            shutil.copy(bck_index, index_path)  
+        else:
+            shutil.copy(index_path, bck_index)  
+        html = str(soup)
+        new_html = html.replace('<head>', '<head>\n' + GA_JS)
+        index_path.write_text(new_html)
+
 
 def init_users_db():
     # Create the Data directory if it doesn't exist
@@ -432,6 +462,7 @@ except ValueError as e:
     st.stop()
 
 def main():
+    inject_ga()
     st.title("Gas Station and Best Price locator")
     # Initialize the database
     init_users_db()
